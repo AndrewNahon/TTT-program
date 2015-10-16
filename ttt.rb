c1c84eb9d6721585dc99bc1df83a3e5192024ed5
Denholm Nahon <andrew.nahon@gmail.com>
2015-10-16 13:12:41 -0700

    Tic Tac Toe

--- ttt.rb
+++ ttt.rb
@@ -1,100 +1,127 @@
-#player one chooses
-#computer chooses
-#check for winner or tie
-#displays board
-#loops
+#ttt game
 
-require 'pry'
+#Psuedo code
+#1. Create an empty board in hash form
+#2. Display board
+#3. Player moves
+  #a. validates move
+    #between 1--9, available square
+  #b.  checks for win or tie
+  #c.  displays board
+#.Computer moves
+  #a. moves to win, to block, or random available space
+  #. check for win or tie
+
+#4. Displays winner
+#5. Ask do you want to play again? Repeat unless no.
+
+WINNING_LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 
 def create_board
-   hash = {}
+  hash = {}
   (1..9).each {|n| hash[n] = ' '}
   hash
-end 
+end
 
-def draw_board(board)
+def display_board(board)
   system 'clear'
-  puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}  "
-  puts "------------------"
-  puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}  "  
-  puts "------------------"
-  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}  "
+  puts " #{board[1]} | #{board[2]} | #{board[3]}"
+  puts "-----------"
+  puts " #{board[4]} | #{board[5]} | #{board[6]}"
+  puts "-----------"
+  puts " #{board[7]} | #{board[8]} | #{board[9]}"
 end
 
-def player_turn(board)
-  puts "Choose square (1--9)"
-  choice = gets.chomp.to_i
-  until board.select {|k, v| v == ' '}.keys.include?(choice)
-    puts "You must pick an empty square."
-    choice = gets.chomp.to_i
+def player_move(board)
+  puts "Make your move. Choose an available square (1--9)"
+  move = gets.chomp.to_i
+  until (1..9).member?(move)
+    puts "You must select an empty number between 1 and 9"
+    move = gets.chomp.to_i
   end
-  board[choice] = 'x'
+  until board[move] == ' '
+    puts "You must select an empty square"
+    move = gets.chomp.to_i
+  end
+  board[move] = 'x'
+  display_board(board)
 end
 
-def comp_turn(board, winning_positions)
-  move1 = two_in_a_row_o(board, winning_positions) 
-  move2 = two_in_a_row_x(board, winning_positions) 
-  choice = board.select {|k,v| v == ' '}.keys.sample
-  move1 ? board[move1] = 'o' : (move2 ? board[move2] = 'o' : board[choice] = 'o')
+def computer_move(board)
+  #check for two in a row
+  if check_for_two(board)
+    move = check_for_two(board)
+    board[move] = 'o'
+  else
+  #pick a random available square
+  move = (board.select {|k, v| v == ' '}.keys.sample)
+  board[move] = 'o'
+  end
+  display_board(board)
 end
 
+def check_for_two(board)
+  WINNING_LINES.each do |line|
+    array = []
+    array += [board[line[0]], board[line[1]], board[line[2]]]
+      if array.count('o') == 2 && array.count(' ') == 1 
+        return line[array.index(' ')]
+      elsif array.count('x') == 2 && array.count(' ') == 1 
+        return line[array.index(' ')]
+      end  
+  end
+  nil
+end
 
-def two_in_a_row_x(board, winning_positions)    
-  winning_positions.each do |position|
-  array = []
-  array = array + [board[position[0]], board[position[1]], board[position[2]]]
-    if array.count('x') == 2 and array.count(' ') == 1
-      move = position[array.index(' ')]
-      return move
+def check_for_winner(board)
+  WINNING_LINES.each do |line|
+    if board[line[0]] == 'o' && board[line[1]] == 'o' && board[line[2]] == 'o'
+      return "Computer"
+    elsif board[line[0]] == 'x' && board[line[1]] == 'x' && board[line[2]] == 'x'
+      return "Player"
     end
   end
   nil
 end
 
-def two_in_a_row_o(board, winning_positions)    
-  winning_positions.each do |position|
-  array = []
-  array = array + [board[position[0]], board[position[1]], board[position[2]]]
-    if array.count('o') == 2 and array.count(' ') == 1
-      move = position[array.index(' ')]
-      return move
-    end
+
+begin 
+
+  board = create_board
+  display_board(board)
+  winner = nil
+
+  begin
+      player_move(board)
+      winner = check_for_winner(board)
+     
+      if board.values.include?(' ') == false || winner 
+        break
+      end
+      computer_move(board)
+      winner = check_for_winner(board)
+      
+      if board.values.include?(' ') == false || winner 
+        break
+      end
+  end until board.values.include?(' ') == false || winner 
+  
+  if winner
+    puts "#{winner} won!"
+  else
+    puts "It's a tie"
   end
-  nil
-end
+
+  puts ''
+  puts "Would you like to play again??"
+  puts "Enter yes or no."
+  play_again = gets.chomp.downcase
+
+end until play_again == 'no' || play_again == 'n'
 
 
-def tie?(board)
-  board.select {|k,v| v == ' '}.keys.empty?
-end
 
-def is_there_a_winner(board, winning_positions)
-  winning_positions.each do |position|
-    if board[position[0]] == 'x' and  board[position[1]] == 'x' and board[position[2]] == 'x'
-      return 'Player'
-    elsif board[position[0]] == 'o' and board[position[1]] == 'o' and board[position[2]] == 'o'
-      return 'Computer'
-    else 
-      return nil
-    end
-  end
-end
 
-winning_positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9],
-                      [3, 5, 7], [3, 5, 7]] 
 
-board = create_board
-draw_board(board)
 
-begin
-  player_turn(board)
-  comp_turn(board, winning_positions)
-  draw_board(board)
-  winner = is_there_a_winner(board, winning_positions)
- end until winner || tie?(board)
 
-if winner
-  puts "#{winner} won!"
-else
- puts "It's a tie."
-end
